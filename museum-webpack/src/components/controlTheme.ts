c015ec47f78c7e00d80131e0e5370023961a02b5
Katia <katia.zakharina@gmail.com>
2022-02-04 10:12:48 +0300

    refactor: rewrite theme control

--- museum-webpack/src/components/controlTheme.ts
+++ museum-webpack/src/components/controlTheme.ts
@@ -1,21 +1,3 @@
-let theme = 'light';
-
-function darkTheme() {
-  document.documentElement.style.setProperty('--theme-light', '#000');
-  document.documentElement.style.setProperty('--theme-dark', '#fff');
-  document.documentElement.style.setProperty('--theme-bg-light', '#030303');
-  document.documentElement.style.setProperty('--theme-gold', '#fff');
-  document.documentElement.style.setProperty('--theme-border', '#fff');
-  theme = 'dark';
-}
-
-function lightTheme() {
-  document.documentElement.style.setProperty('--theme-light', '#fff');
-  document.documentElement.style.setProperty('--theme-dark', '#000');
-  document.documentElement.style.setProperty('--theme-bg-light', '#fff');
-  document.documentElement.style.setProperty('--theme-gold', '#9d8665');
-  document.documentElement.style.setProperty('--theme-border', '#000');
-  theme = 'light';
-}
-
-document.querySelector('.header_theme').addEventListener('click', () => (theme === 'light' ? darkTheme() : lightTheme()));
+document.querySelector('.header_theme').addEventListener('click', () => {
+  document.body.dataset.theme = document.body.dataset.theme === 'light' ? 'dark' : 'light';
+});
